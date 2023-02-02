f29bdda04a6c1e0e8bc3c5b8e5882a3e2d315b38
MaxTsarev <maxtsar33@gmail.com>
2023-02-02 20:10:44 +0300

    Added Extract method

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,6 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void printList(List<String> list) {
-        int i = 1;
-        for (String s : list) {
-            System.out.println(i + ". " + s);
-            i++;
-        }
-    }
-
     public static void main(String[] args) {
 
         List<String> list = new ArrayList<>();
@@ -26,47 +18,65 @@ public class Main {
             int number = scanner.nextInt();
 
             if (number == 1) {
-                System.out.println("Какую покупку хотите добавить?");
-                String line = scanner1.nextLine();
-                list.add(line);
-                System.out.println("Итого в списке покупок: " + list.size());
+                addProduct(list, scanner1);
             }
-
             if (number == 2) {
                 printList(list);
             }
-
             if (number == 3) {
-                printList(list);
-                System.out.println("Какую покупку хотите удалить? Введите название или номер:");
-                String line = scanner1.nextLine();
-
-                try {
-                    int opNum = Integer.parseInt(line) - 1;
-                    list.remove(opNum);
-                    int a = 1;
-                    printList(list);
-                } catch (NumberFormatException e) {
-                    list.remove(line);
-                    printList(list);
-                }
+                remProduct(list, scanner1);
             }
-
             if (number == 4) {
-                System.out.println("Введите текст для поиска:");
-                String line = scanner1.nextLine();
-                String queryLower = line.toLowerCase();
+                searchProduct(list, scanner1);
+            }
+        }
+    }
 
-                for (int i = 0; i < list.size(); i++) {
-                    String itLower = list.get(i);
-                    String itemLower = itLower.toLowerCase();
-                    if (itemLower.contains(queryLower)) {
-                        i++;
-                        System.out.println(i + ". " + itLower);
-                    } else {
-                        System.out.println("Нет товара с похожим названием!");
-                    }
-                }
+    public static void printList(List<String> list) {
+        int i = 1;
+        for (String s : list) {
+            System.out.println(i + ". " + s);
+            i++;
+        }
+    }
+
+    private static void addProduct(List<String> list, Scanner scanner1) {
+        System.out.println("Какую покупку хотите добавить?");
+        String line = scanner1.nextLine();
+        list.add(line);
+        System.out.println("Итого в списке покупок: " + list.size());
+    }
+
+    private static void remProduct(List<String> list, Scanner scanner1) {
+        printList(list);
+        System.out.println("Какую покупку хотите удалить? Введите название или номер:");
+        String line = scanner1.nextLine();
+
+
+        try {
+            int opNum = Integer.parseInt(line) - 1;
+            list.remove(opNum);
+            int a = 1;
+            printList(list);
+        } catch (NumberFormatException e) {
+            list.remove(line);
+            printList(list);
+        }
+    }
+
+    private static void searchProduct(List<String> list, Scanner scanner1) {
+        System.out.println("Введите текст для поиска:");
+        String line = scanner1.nextLine();
+        String queryLower = line.toLowerCase();
+
+        for (int i = 0; i < list.size(); i++) {
+            String itLower = list.get(i);
+            String itemLower = itLower.toLowerCase();
+            if (itemLower.contains(queryLower)) {
+                i++;
+                System.out.println(i + ". " + itLower);
+            } else {
+                System.out.println("Нет товара с похожим названием!");
             }
         }
     }
